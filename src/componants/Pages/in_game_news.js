4e79d96bdc13c4fd0df0473f56f1cd262876992e
Jason Charnley <jason.charnley@LDNML4TXJH6.clients.global.arup.com>
2019-07-04 11:38:36 +0100

    #14 added news on homepage and news route

--- src/componants/Pages/in_game_news.js
+++ src/componants/Pages/in_game_news.js
@@ -7,7 +7,6 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-// import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 
@@ -22,19 +21,13 @@ import {
 
 const News = ({ news, location }) => {
 
-  const [value, setvalue] = useState(false);
+  // const [value, setvalue] = useState(false);
 
-  const onChange = (input) => {
-      setvalue(input);
-  };
+  // const onChange = (input) => {
+  //     setvalue(input);
+  // };
 
-
-
-// console.log("this is location", location.location.pathname)
-
-const displayNews = news && news.data.map((newsObj, key) => {
-  news.data = location.location.pathname === "/" ? news.data.slice(0, 3) : news.data;
-
+const displayNews = location.location.pathname === '/' ? news && news.data.slice(0,3).map((newsObj, key) => {
     const { title, body, image, time, meta} = newsObj;
     return ( 
       <div key={key}>
@@ -82,11 +75,58 @@ const displayNews = news && news.data.map((newsObj, key) => {
    </Card>
         </div>
     )
-})
+}) : news && news.data.map((newsObj, key) => {
+      const { title, body, image, time, meta} = newsObj;
+      return ( 
+        <div key={key}>
+          <Card>
+       <CardActionArea>
+         <CardMedia
+           component="img"
+           alt={title}
+           height="140"
+           image={image}
+           title={title}
+         />
+         <CardContent>
+           <Typography gutterBottom variant="h5" component="h2">
+            {title}
+           </Typography>
+           <Typography variant="body2" color="textSecondary" component="p">
+           {body}
+           </Typography>
+         </CardContent>
+       </CardActionArea>
+       <CardActions>
+             <FacebookShareButton
+                 url={image}
+                 quote={body}
+                 hashtag={'#unvaulted'}
+                 style={{marginLeft: "10px"}}
+                 className="Demo__some-network__share-button">
+                 <FacebookIcon
+                   size={25}
+                   round />
+               </FacebookShareButton>
+               <TwitterShareButton
+                 url={'https://unvaulted.co.uk'}
+                 title={title}
+                 via={body}
+                 hashtag={'#unvaulted'}
+                 style={{marginLeft: "10px"}}
+                 className="Demo__some-network__share-button">    
+                 <TwitterIcon
+                   size={25}
+                   round />
+               </TwitterShareButton>
+       </CardActions>
+     </Card>
+          </div>
+      )
+  })
     return (
        <div>
            {displayNews}
-           {/* <button onClick={onChange(true)}>Click to show more news!</button> */}
        </div>
     );
   };
